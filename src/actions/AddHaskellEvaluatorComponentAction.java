24bf56152ae70afe2eb870ab664b92f43abf64e0
UltimatePea <superchenzb@gmail.com>
2017-03-02 19:02:22 -0800

    Add Basic Haskell Language

---
+++ src/actions/AddHaskellEvaluatorComponentAction.java
@@ -0,0 +1,30 @@
+package actions;
+
+import actions.menu.ActionsMenuBarTitles;
+import paintcomponents.haskell.EvaluateHaskellPaintComponent;
+import ui.PaintPanel;
+
+public class AddHaskellEvaluatorComponentAction extends PaintAction {
+
+	public AddHaskellEvaluatorComponentAction(PaintPanel panel) {
+		super(panel);
+	}
+
+	@Override
+	public boolean canPerformAction() {
+		return true;
+	}
+
+	@Override
+	public void performAction() {
+		EvaluateHaskellPaintComponent comp = new EvaluateHaskellPaintComponent("Use Data Display/Update to compute expression result", panel.getWidth() /2, panel.getHeight()/2);
+		panel.addPaintComponent(comp);
+		panel.repaint();
+	}
+
+	@Override
+	public String locationString() {
+		return ActionsMenuBarTitles.Developer("Haskell/Add Evaluator").toString();
+	}
+
+}
