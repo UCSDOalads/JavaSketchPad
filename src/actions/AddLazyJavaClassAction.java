268aac7b1f8bbdde9d0b32946c1ce02f422732e2
lhwlyd <874036568@qq.com>
2017-03-25 17:56:40 -0700

    Intergrate Java Search

--- src/actions/AddLazyJavaClassAction.java
+++ src/actions/AddLazyJavaClassAction.java
@@ -1,5 +1,8 @@
 package actions;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 import actions.edit.undoredo.SharedUndoRedoActionManager;
@@ -8,6 +11,9 @@ import actions.menu.ActionsMenuBarTitles;
 import actions.menu.PaintActionMenuItem;
 import paintcomponents.java.lazy.ClassPaintComponent;
 import ui.PaintPanel;
+
+import ui.helper.ClassSearchFrame;
+import ui.helper.ClassSearchFrameDelegateInterface;
 
 public class AddLazyJavaClassAction extends PaintAction {
 
@@ -22,37 +28,59 @@ public class AddLazyJavaClassAction extends PaintAction {
 
 	@Override
 	public void performAction() {
-		String className = JOptionPane
-				.showInputDialog("Please specify the name of the Java Class");
-		try {
-			Class classObj = Class.forName(className);
-			ClassPaintComponent comp = new ClassPaintComponent(classObj,
-					panel.getWidth() / 2, panel.getHeight() / 2);
-			panel.addPaintComponent(comp);
-			// add action to undo redo manager
-			SharedUndoRedoActionManager.getSharedInstance().pushUndoableAction(new UndoRedoableInterface() {
+		ClassSearchFrame searchFrame = new ClassSearchFrame(this);
+		searchFrame.setDelegate(new ClassSearchFrameDelegateInterface() {
+			
+			@Override
+			public void didSelectClass(String classname) {
+				// TODO Auto-generated method stub
 				
-				@Override
-				public void undoAction() {
-					comp.remove(panel);
-					panel.repaint();
-				}
-				
-				@Override
-				public void redoAction() {
-					panel.addPaintComponent(comp);
-					panel.repaint();
-				}
-			});
-			panel.repaint();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			JOptionPane.showMessageDialog(panel,
-					className + " :: Class Not Found");
-		}
-
+			}
+		});
+		
+		searchFrame.setVisible(true);
+		searchFrame.setSize(new Dimension(300,200));
+		
+		panel.repaint();
+	
 	}
-
+	
+	/*
+	 * Add a new class to the painting area when it receives a call
+	 * from the search frame.
+	 */
+	public void perform(String name){
+		String className = name;
+			
+			try {
+				
+				Class classObj = Class.forName(className);
+				ClassPaintComponent comp = new ClassPaintComponent(classObj,
+						panel.getWidth() / 2, panel.getHeight() / 2);
+				panel.addPaintComponent(comp);
+				// add action to undo redo manager
+				SharedUndoRedoActionManager.getSharedInstance().pushUndoableAction(new UndoRedoableInterface() {
+					
+					@Override
+					public void undoAction() {
+						comp.remove(panel);
+						panel.repaint();
+					}
+					
+					@Override
+					public void redoAction() {
+						panel.addPaintComponent(comp);
+						panel.repaint();
+					}
+				});
+				panel.repaint();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(panel,
+						className + " :: Class Not Found");
+			}
+	}
+	
 	@Override
 	public String locationString() {
 		return ActionsMenuBarTitles.Lazy().Add().Java_Class().toString();
