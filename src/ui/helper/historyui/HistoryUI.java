4610907f0facf61bd5e6d8ff16e234bfd6774c9b
heheheheheh <kaijiec@outlook.com>
2017-03-29 15:29:20 -0700

    comment note

--- src/ui/helper/historyui/HistoryUI.java
+++ src/ui/helper/historyui/HistoryUI.java
@@ -22,7 +22,10 @@ import javax.swing.JButton;
 
 
 
-
+/**
+ * create historyUI
+ *
+ */
 public class HistoryUI extends JFrame
 		{
 	private static final long serialVersionUID = 8141494344180865577L;
@@ -37,8 +40,10 @@ public class HistoryUI extends JFrame
 	@SuppressWarnings("rawtypes")
 	private Stack<ArrayList> delete_history;
 
-	/*
+	/**
 	 * Setup historyUI
+	 * 
+	 * param: receive a list of button names
 	 */
 	public HistoryUI(String[] titles) {
 
@@ -67,21 +72,23 @@ public class HistoryUI extends JFrame
 		scrollPane = new JScrollPane(resultsTable);
 		panel.add(scrollPane, BorderLayout.CENTER);
 
-		// add small JPanel for Cancel and Confirm buttons
+		// add small JPanel for buttons
 		JPanel button_panel = new JPanel();
 		getContentPane().add(button_panel, BorderLayout.SOUTH);
 		
 		//initialize buttons
 		createButtons(titles, button_panel);
 		
+		//stack, used to stored deleted rows
 		delete_history = new Stack<>();
 	}
 
-	/*
+	/**
 	 * create buttons
 	 * param: a list of buttons' name and button panel
 	 */
 	public void createButtons(String[] titles, JPanel button_panel){
+		//loop through arr and add buttons
 		for (String title : titles){
 			JButton button = new JButton(title);
 			button_panel.add(button);
@@ -100,12 +107,12 @@ public class HistoryUI extends JFrame
 						System.exit(EXIT_ON_CLOSE);
 					}
 					
-					//if revert button pressed
+					//if revert button pressed, revert to last version
 					else if(title =="revert"){
 						revert();
 					}
 					
-					//if clear button pressed
+					//if clear button pressed, delete all rows in table
 					else if(title =="clear"){
 						int num_rows = resultsTable.getRowCount();
 						int[] indices = IntStream.range(0, num_rows).toArray();
@@ -119,15 +126,19 @@ public class HistoryUI extends JFrame
 	}
 
 
-	/*
+	/**
 	 * revert deleted rows
 	 */
 	@SuppressWarnings("rawtypes")
 	public void revert(){
+		
 		if(!delete_history.isEmpty()){
 			ArrayList arr = delete_history.pop();
+			//get the deleted rows and corresponding data 
 			int[] indices = (int[]) arr.get(0);
 			HistoryDataObject[] objects = (HistoryDataObject[]) arr.get(1);
+			
+			//insert data to its row
 			for(int i = 0; i<indices.length; i++){
 				defaultTableModel.insertRow(indices[i], new Object[] {objects[i]} );
 			}
@@ -136,7 +147,7 @@ public class HistoryUI extends JFrame
 	
 	
 	
-	/*
+	/**
 	 * add a row to the end of table
 	 * receive a HistoryDataObject
 	 */
@@ -145,22 +156,24 @@ public class HistoryUI extends JFrame
 	}
 	
 
-	/*
+	/**
 	 * remove several selected rows from table and stores them into stack
 	 * param: an array of rows number to be removed
 	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void removeSeveralRows(int[] indices){
-		//create a arr to hold the deleted data and rows
+		//create a arr to hold the deleted rows and its data
 		ArrayList arr = new ArrayList<>();
 		HistoryDataObject[] objects = new HistoryDataObject[indices.length];
 		Arrays.sort(indices);
 		
+		//loop through indices and delete
 		for(int i=indices.length-1;i>=0;i--){
 			objects[i] = (HistoryDataObject) resultsTable.getModel().getValueAt(indices[i], 0);
 			defaultTableModel.removeRow(indices[i]);
 		}
 		
+		//stores deleted rows and its data to delete_history stack
 		arr.add(indices);
 		arr.add(objects);
 		delete_history.push(arr);
@@ -170,8 +183,8 @@ public class HistoryUI extends JFrame
 		this.delegate = delegate;
 	}
 
-	/*
-	 * Forcedly remove single selection in JTable
+	/**
+	 * Forcedly multiple interval selection in JTable
 	 */
 	private static class ForcedListSelectionModel
 			extends
