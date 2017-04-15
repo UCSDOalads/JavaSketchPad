f8efe6f67a3e76ff04f6f279f2554292c060eebc
chihenkoian <chk111@ucsd.edu>
2017-04-15 16:26:08 -0700

    Test Case

--- src/actions/global/globalactions/AddLazyJavaFieldsComponentGlobalAction.java
+++ src/actions/global/globalactions/AddLazyJavaFieldsComponentGlobalAction.java
@@ -4,7 +4,7 @@ import actions.global.GlobalPaintAction;
 import ui.PaintPanel;
 
 public class AddLazyJavaFieldsComponentGlobalAction extends GlobalPaintAction {
-
+        private Class 
 	@Override
 	protected void execute(PaintPanel panel) {
 		// TODO Auto-generated method stub
--- src/actions/global/globalactions/AddLazyJavaMethodComponentGlobalAction.java
+++ src/actions/global/globalactions/AddLazyJavaMethodComponentGlobalAction.java
@@ -4,6 +4,10 @@ import actions.global.GlobalPaintAction;
 import ui.PaintPanel;
 
 public class AddLazyJavaMethodComponentGlobalAction extends GlobalPaintAction {
+    private Class classToCreate;
+
+    public void setClassToCreate(Class classToCreate){
+	
 
 	@Override
 	protected void execute(PaintPanel panel) {
