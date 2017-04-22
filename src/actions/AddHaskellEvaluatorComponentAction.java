d7c28fa4e91be6171a14b43a6b24eb1cec0df573
cvShokaku <496428927@qq.com>
2017-04-22 14:24:40 -0700

    update addHaskellEvaluatorComponentAction

--- src/actions/AddHaskellEvaluatorComponentAction.java
+++ src/actions/AddHaskellEvaluatorComponentAction.java
@@ -1,12 +1,10 @@
 package actions;
 
+import ui.PaintPanel;
 import actions.global.ActionName;
 import actions.global.GlobalPaintActionExecuter;
 import actions.global.globalactions.AddHaskellEvaluatorComponentGlobalAction;
 import actions.menu.ActionsMenuBarTitles;
-import ui.PaintPanel;
-import ui.general.InputManager;
-import ui.general.InputManagerDelegate;
 
 public class AddHaskellEvaluatorComponentAction extends MenuBarPaintAction {
 
@@ -21,18 +19,10 @@ public class AddHaskellEvaluatorComponentAction extends MenuBarPaintAction {
 
 	@Override
 	public void performAction() {
-		InputManager im = new InputManager();
-		im.askForClass(panel, new InputManagerDelegate<Class>() {
-
-			@Override
-			public void didFinishInput(Class input) {
-				AddHaskellEvaluatorComponentGlobalAction assiciatedAction = (AddHaskellEvaluatorComponentGlobalAction) ActionName.ADD_HASKELL_EVALUATOR_COMPONENT
+		AddHaskellEvaluatorComponentGlobalAction assiciatedAction = (AddHaskellEvaluatorComponentGlobalAction) ActionName.ADD_HASKELL_EVALUATOR_COMPONENT
 						.getAssiciatedAction();
-				assiciatedAction.setClassToCreate(input);
-				GlobalPaintActionExecuter.getSharedInstance().execute(assiciatedAction, panel);
-
-			}
-		});
+		GlobalPaintActionExecuter.getSharedInstance().execute(assiciatedAction,
+				panel);
 	}
 
 	@Override
