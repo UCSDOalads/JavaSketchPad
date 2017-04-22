4f29a213bb6487db92cebf32113858e1d2ca1078
zhc180 <zhc180@ucsd.edu>
2017-04-22 14:11:54 -0700

    Remove comments

--- src/actions/global/globalactions/AddInstanceOperationGlobalAction.java
+++ src/actions/global/globalactions/AddInstanceOperationGlobalAction.java
@@ -2,46 +2,20 @@ package actions.global.globalactions;
 
 import java.lang.reflect.Constructor;
 
-import actions.global.GlobalPaintAction;
 import paintcomponents.java.interactive.InstanceOperationComponent;
 import ui.PaintPanel;
+import actions.global.GlobalPaintAction;
 
 public class AddInstanceOperationGlobalAction extends GlobalPaintAction {
-	
+
 	private Constructor constructorToSet;
 	private InstanceOperationComponent insComp;
-	
+
 	@Override
 	protected void execute(PaintPanel panel) {
-		insComp = new InstanceOperationComponent(
-				constructorToSet, panel.getWidth() / 2,
-				panel.getHeight() / 2);
+		insComp = new InstanceOperationComponent(constructorToSet,
+				panel.getWidth() / 2, panel.getHeight() / 2);
 		panel.addPaintComponent(insComp);
-		// add action to undo redo manager
-//		SharedUndoRedoActionManager.getSharedInstance().pushUndoableAction(new UndoRedoableInterface() {
-//					
-//			@Override
-//			public void undoAction() {
-//				consComp.remove(panel);
-//				panel.repaint();
-//			}
-//					
-//			@Override
-//			public void redoAction() {
-//				panel.addPaintComponent(consComp);
-//				panel.repaint();
-//			}
-//
-//			@Override
-//			protected String commandName() {
-//				return "NOT YET IMPLEMENTED";
-//			}
-//
-//			@Override
-//			protected String commandDescription() {
-//				return "NOT YET IMPLEMENTED";
-//			}
-//		});
 		panel.repaint();
 	}
 
