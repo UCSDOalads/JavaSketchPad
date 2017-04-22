adaa21a045bf80e49f48df3779245ea75ce4342e
UltimatePea <superchenzb@gmail.com>
2017-04-22 15:15:20 -0700

    Fix all tests

--- src/actions/global/globalactions/AddHaskellComponentGlobalAction.java
+++ src/actions/global/globalactions/AddHaskellComponentGlobalAction.java
@@ -1,31 +1,29 @@
 package actions.global.globalactions;
 
-import javax.swing.JOptionPane;
-
 import actions.global.GlobalPaintAction;
 import paintcomponents.haskell.HaskellExpressionPaintComponent;
 import ui.PaintPanel;
 
 public class AddHaskellComponentGlobalAction extends GlobalPaintAction {
 
-	private Class classToCreate;
 
-	/**
-	 * @param classToCreate
-	 *            the classToCreate to set
-	 */
-	public void setClassToCreate(Class classToCreate) {
-		this.classToCreate = classToCreate;
-	}
-
+	private String haskellExpression;
 	@Override
 	protected void execute(PaintPanel panel) {
 		// TODO Auto-generated method stub
 
-		String expr = JOptionPane.showInputDialog("Please enter the haskell expression");
+		String expr = haskellExpression;
 		panel.addPaintComponent(new HaskellExpressionPaintComponent(expr, panel.getWidth() / 2, panel.getHeight() / 2));
 
 		panel.repaint();
+	}
+
+	/**
+	 * @param haskellExpression
+	 *            the haskellExpression to set
+	 */
+	public void setHaskellExpression(String haskellExpression) {
+		this.haskellExpression = haskellExpression;
 	}
 
 }
