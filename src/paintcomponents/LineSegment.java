a61a8b628589cddc38b454fa287a268cf7baf5b2
sutan1206 <sutan1206@gmail.com>
2017-05-20 14:49:42 -0700

    Formatted

--- src/paintcomponents/LineSegment.java
+++ src/paintcomponents/LineSegment.java
@@ -68,14 +68,15 @@ public class LineSegment extends PaintComponent {
 
 	/**
 	 * Constructs a line segment with default width and default height.
+	 * 
 	 * @param fromPoint
 	 * @param toPoint
 	 */
 	public LineSegment(SimplePoint fromPoint, SimplePoint toPoint) {
-		this(fromPoint, toPoint,
-				Defaults.sharedDefaults().defaultColorForLineSegment(),
-				Defaults.sharedDefaults().defaultColorForSelectedLineSegment(),
-				Defaults.sharedDefaults().defaultStrokeWidthForLineSegment());
+		this(fromPoint, toPoint, Defaults.sharedDefaults()
+				.defaultColorForLineSegment(), Defaults.sharedDefaults()
+				.defaultColorForSelectedLineSegment(), Defaults
+				.sharedDefaults().defaultStrokeWidthForLineSegment());
 
 	}
 
@@ -102,13 +103,17 @@ public class LineSegment extends PaintComponent {
 		if (fromPoint.contains(x, y) || toPoint.contains(x, y)) {
 			return false;
 		}
-		
+
 		double lineXDis = fromPoint.getX() - toPoint.getX();
 		double lineYDis = toPoint.getY() - fromPoint.getY();
-		double constantTerm = fromPoint.getY() * toPoint.getX() - toPoint.getY() * fromPoint.getX();
-		
+		double constantTerm = fromPoint.getY() * toPoint.getX()
+				- toPoint.getY() * fromPoint.getX();
+
 		// Calculate point to line distance with formula.
-		double pointToLineDis = Math.abs((lineYDis * x + lineXDis * y + constantTerm) / Math.sqrt(Math.pow(lineYDis, 2) + Math.pow(lineXDis, 2)));
+		double pointToLineDis = Math
+				.abs((lineYDis * x + lineXDis * y + constantTerm)
+						/ Math.sqrt(Math.pow(lineYDis, 2)
+								+ Math.pow(lineXDis, 2)));
 
 		// If the point is within 3 pixels of the line, return true.
 		if (pointToLineDis <= 3 + strokeWidth / 2) {
@@ -122,20 +127,20 @@ public class LineSegment extends PaintComponent {
 	public void translate(int i, int j) {
 		super.translate(i, j);
 		// if from and to points are not selected, translate them as well
-		if (this.fromPoint.isSelected() == false){
+		if (this.fromPoint.isSelected() == false) {
 			this.fromPoint.translate(i, j);
 		}
-		if (this.toPoint.isSelected() == false){
+		if (this.toPoint.isSelected() == false) {
 			this.toPoint.translate(i, j);
 		}
 
 	}
-	
+
 	@Override
 	public void saveToElement(Element rootElement, Document doc) {
 		super.saveToElement(rootElement, doc);
-		
-		//build elements
+
+		// build elements
 		Element main = doc.createElement("linesegment");
 
 		Element fromPointElem = doc.createElement("frompoint");
@@ -143,65 +148,77 @@ public class LineSegment extends PaintComponent {
 		Element defaultColorElem = doc.createElement("defaultcolor");
 		Element selectColorElem = doc.createElement("selectcolor");
 		Element strokeWidthElem = doc.createElement("strokewidth");
-		
-		//append relationship
+
+		// append relationship
 		main.appendChild(fromPointElem);
 		main.appendChild(toPointElem);
 		main.appendChild(defaultColorElem);
 		main.appendChild(selectColorElem);
 		main.appendChild(strokeWidthElem);
 		rootElement.appendChild(main);
-		
-		//fill data
-		fromPointElem.setAttribute("id", Long.toString(fromPoint.getComponentID()));
+
+		// fill data
+		fromPointElem.setAttribute("id",
+				Long.toString(fromPoint.getComponentID()));
 		toPointElem.setAttribute("id", Long.toString(toPoint.getComponentID()));
-		XMLEncodingUtilForPaintComponents.attachRGB(defaultColor, defaultColorElem, doc);
-		XMLEncodingUtilForPaintComponents.attachRGB(selectColor, selectColorElem, doc);
-		strokeWidthElem.appendChild(doc.createTextNode(Double.toString(strokeWidth)));
-		
+		XMLEncodingUtilForPaintComponents.attachRGB(defaultColor,
+				defaultColorElem, doc);
+		XMLEncodingUtilForPaintComponents.attachRGB(selectColor,
+				selectColorElem, doc);
+		strokeWidthElem.appendChild(doc.createTextNode(Double
+				.toString(strokeWidth)));
+
 	}
 
 	/**
 	 * 
-	 * Recover the line segment from a element that should be operated by saveToElement
+	 * Recover the line segment from a element that should be operated by
+	 * saveToElement
 	 * 
-	 * The implementation searches all panel's paintcomponents to find the matching ids using PanelIO.idMapping
+	 * The implementation searches all panel's paintcomponents to find the
+	 * matching ids using PanelIO.idMapping
 	 * 
 	 * @param rootElement
 	 * @param panel
 	 */
 	public LineSegment(Element rootElement, PaintPanel panel) {
 		super(rootElement, panel);
-		Element main = (Element) rootElement.getElementsByTagName("linesegment").item(0);
-		Element fromPointElement = (Element) main.getElementsByTagName("frompoint").item(0);
-		Element toPointElem = (Element) main.getElementsByTagName("topoint").item(0);
-		Element defaultColorElem = (Element) main.getElementsByTagName("defaultcolor").item(0);
-		Element selectColorElem = (Element) main.getElementsByTagName("selectcolor").item(0);
-		Element strokeWidthElem = (Element) main.getElementsByTagName("strokewidth").item(0);
-		
-		
-		defaultColor = XMLEncodingUtilForPaintComponents.getRGB(defaultColorElem);
+		Element main = (Element) rootElement
+				.getElementsByTagName("linesegment").item(0);
+		Element fromPointElement = (Element) main.getElementsByTagName(
+				"frompoint").item(0);
+		Element toPointElem = (Element) main.getElementsByTagName("topoint")
+				.item(0);
+		Element defaultColorElem = (Element) main.getElementsByTagName(
+				"defaultcolor").item(0);
+		Element selectColorElem = (Element) main.getElementsByTagName(
+				"selectcolor").item(0);
+		Element strokeWidthElem = (Element) main.getElementsByTagName(
+				"strokewidth").item(0);
+
+		defaultColor = XMLEncodingUtilForPaintComponents
+				.getRGB(defaultColorElem);
 		selectColor = XMLEncodingUtilForPaintComponents.getRGB(selectColorElem);
 		strokeWidth = Double.parseDouble(strokeWidthElem.getTextContent());
 		stroke = new BasicStroke((float) strokeWidth);
-		
-		//append from and to points
-		
+
+		// append from and to points
+
 		ArrayList<PaintComponent> paintComponents = panel.getPaintComponents();
-		
-		
-		//select only the paint components to check ID
-		long fromPointID = PanelIO.idMapping.get(Long.parseLong(fromPointElement.getAttribute("id")));
-		long toPointID = PanelIO.idMapping.get(Long.parseLong(toPointElem.getAttribute("id")));
-		
+
+		// select only the paint components to check ID
+		long fromPointID = PanelIO.idMapping.get(Long
+				.parseLong(fromPointElement.getAttribute("id")));
+		long toPointID = PanelIO.idMapping.get(Long.parseLong(toPointElem
+				.getAttribute("id")));
+
 		for (PaintComponent paintComponent : paintComponents) {
-			if(paintComponent.getComponentID() == fromPointID){
+			if (paintComponent.getComponentID() == fromPointID) {
 				fromPoint = (SimplePoint) paintComponent;
-			} else if (paintComponent.getComponentID() == toPointID){
-				toPoint  = (SimplePoint) paintComponent;
+			} else if (paintComponent.getComponentID() == toPointID) {
+				toPoint = (SimplePoint) paintComponent;
 			}
 		}
 	}
-	
 
 }
