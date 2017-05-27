d0b960e071cf88fe7c4d9722abf418154cbb26cb
Yidong Luo <874036568@qq.com>
2017-05-27 15:52:50 -0700

    Fixed the assiciated naming problem in the new added files

--- src/actions/AddInteactiveJavaMethodComponentAction.java
+++ src/actions/AddInteactiveJavaMethodComponentAction.java
@@ -52,15 +52,15 @@ public class AddInteactiveJavaMethodComponentAction extends MenuBarPaintAction {
 		//call DialogInputChecker to check input
 		DialogInputChecker inputChecker = new DialogInputChecker();
 		if(inputChecker.isValidNumber(desiaredConstructorIndex, 0, methods.length -1)){
-			AddInteractiveJavaMethodComponentGlobalAction assiciatedAction 
-			= (AddInteractiveJavaMethodComponentGlobalAction) ActionName.ADD_LAZY_JAVA_METHOD_ACTION.getAssiciatedAction();
-			assiciatedAction.setMethodComponent(comp);
-			assiciatedAction.setMethod(comp.getDisplayingClass().getMethods()[Integer.parseInt(desiaredConstructorIndex)]);
+			AddInteractiveJavaMethodComponentGlobalAction associatedAction 
+			= (AddInteractiveJavaMethodComponentGlobalAction) ActionName.ADD_LAZY_JAVA_METHOD_ACTION.getAssociatedAction();
+			associatedAction.setMethodComponent(comp);
+			associatedAction.setMethod(comp.getDisplayingClass().getMethods()[Integer.parseInt(desiaredConstructorIndex)]);
 			
 			//Need to add: set instance
 			
 			
-			GlobalPaintActionExecuter.getSharedInstance().execute(assiciatedAction, panel);
+			GlobalPaintActionExecuter.getSharedInstance().execute(associatedAction, panel);
 		}
 	}
 		
