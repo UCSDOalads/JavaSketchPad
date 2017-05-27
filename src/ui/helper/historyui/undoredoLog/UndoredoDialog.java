d8db72601081fefce3fffa6fd474dfe67d76a9df
Cheng Gong <chenggong0328@gmail.com>
2017-05-27 15:17:51 -0700

    Deleted Unneeded lines

--- src/ui/helper/historyui/undoredoLog/UndoredoDialog.java
+++ src/ui/helper/historyui/undoredoLog/UndoredoDialog.java
@@ -144,11 +144,6 @@ public class UndoredoDialog extends TableUITemplate implements TableUITemplateIn
 		}
 	}
 	
-	/*
-	public void redoContinuousRow(){
-		int selected
-	}*/
-	
 	/**
 	 * disable undoButton when there is no actions to undo
 	 * disable redoButton when there is no actions to redo
